2dd39cb19d903640f58283d7b4bc6b9c4e2ae107
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-10-17 22:33:04 -0500

    A star wip

--- Assets/Scripts/BearManager.cs
+++ Assets/Scripts/BearManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BearManager : MonoBehaviour
 {
     private BearController selectedBear = null;
+    public GameObject mainTilemap; // This is the walkable tiles
+    public Camera camera;
     public BearController SelectedBear
     {
         get => selectedBear;
@@ -33,11 +36,61 @@ public class BearManager : MonoBehaviour
         
     }
 
+    Vector2Int PosOnGrid(Tilemap grid, Vector3 worldPos)
+    {
+        // Get the position on the grid
+        Vector3 positionRelativeToGrid = mainTilemap.transform.InverseTransformPoint(worldPos);
+        return new Vector2Int((int)(positionRelativeToGrid.x / (int)grid.cellSize.x * (int)grid.cellSize.x), (int)(positionRelativeToGrid.y / (int)grid.cellSize.y * (int)grid.cellSize.y));
+    }
+
+    TileBase TileAtGridPos(Tilemap grid, Vector2Int gridPos)
+    {
+        return grid.GetTile(new Vector3Int(gridPos.x, gridPos.y, 0));
+    }
+
+    TileBase TileAtWorldPos(Tilemap grid, Vector3 worldPos)
+    {
+        return TileAtGridPos(grid, PosOnGrid(grid, worldPos));
+    }
+
+    int EdgeWeight(Vector2Int a, Vector2Int b)
+    {
+        return 1;
+    }
+
     void Update()
     {
         if (!SelectedAnyBearThisFrame && Input.GetMouseButtonDown(0)) // Left mouse click
         {
             // This should run before any bear receives the click event
+
+            if (SelectedBear != null)
+            {
+                // Give pathfinding command at the position of the click, if the tile is reachable etc.
+                //print(Input.mousePosition);
+                Tilemap grid = mainTilemap.GetComponent<Tilemap>();
+                // Get our click position in the world
+                Vector3 position = camera.ScreenToWorldPoint(Input.mousePosition);
+                // Get our click position on the grid and then get the tile at that position
+                Vector2Int clickCellPos = PosOnGrid(grid, position);
+                // Get tile at click position
+                TileBase tileDest = TileAtGridPos(grid, clickCellPos);
+
+                // Get our bear's position on the grid and then get the tile at that position
+                Vector2Int bearCellPos = PosOnGrid(grid, SelectedBear.transform.position);
+                // Get tile at bear position
+                TileBase tileBear = TileAtGridPos(grid, bearCellPos);
+
+                print("Pathfinding from " + bearCellPos + " to " + clickCellPos);
+                IList<Vector2Int> pathToFollow = AStar.A_Star(EdgeWeight, bearCellPos, clickCellPos, new Vector2Int(grid.size.x, grid.size.y));
+                string result = "";
+                foreach (var item in pathToFollow)
+                {
+                    result += item.ToString() + ", ";
+                }
+                print(result);
+            }
+
             // Deselect any selected bears
             SelectedBear = null;
         }
--- Assets/Scripts/Lib/AStar.cs
+++ Assets/Scripts/Lib/AStar.cs
@@ -14,10 +14,10 @@ class AStar
         return Math.Abs(a.x - b.x) + Math.Abs(a.y - b.y);
     }
 
-    static int edgeWeight(Vector2Int a, Vector2Int b)
-    {
-        return 1;
-    }
+    //static int edgeWeight(Vector2Int a, Vector2Int b)
+    //{
+    //    return 1;
+    //}
 
     // It is returned in reverse.
     static IList<Vector2Int> reconstructPath(Dictionary<Vector2Int, Vector2Int> cameFrom, Vector2Int current)
@@ -32,7 +32,7 @@ class AStar
         return totalPath;
     }
 
-    static void process(Vector2Int neighbor, Dictionary<Vector2Int, int> gScore, Dictionary<Vector2Int, int> fScore, Dictionary<Vector2Int, Vector2Int> cameFrom, PriorityQueue<int, Vector2Int> openSet, Vector2Int current, Vector2Int goal)
+    static void process(Func<Vector2Int, Vector2Int, int> edgeWeight, Vector2Int neighbor, Dictionary<Vector2Int, int> gScore, Dictionary<Vector2Int, int> fScore, Dictionary<Vector2Int, Vector2Int> cameFrom, PriorityQueue<int, Vector2Int> openSet, Vector2Int current, Vector2Int goal)
     {
         // d(current,neighbor) is the weight of the edge from current to neighbor
         // tentative_gScore is the distance from start to the neighbor through current
@@ -58,7 +58,7 @@ class AStar
     // Returns the empty list if no path was found between `start` and `goal`.
     // A* finds a path from start to goal.
     // h is the heuristic function. h(n) estimates the cost to reach goal from node n.
-    static IList<Vector2Int> A_Star(Vector2Int start, Vector2Int goal, Vector2Int tilemapDimensions)
+    public static IList<Vector2Int> A_Star(Func<Vector2Int, Vector2Int, int> edgeWeight, Vector2Int start, Vector2Int goal, Vector2Int tilemapDimensions)
     {
         // The set of discovered nodes that may need to be (re-)expanded.
         // Initially, only the start node is known.
@@ -93,19 +93,19 @@ class AStar
             // For all neighbors that exist:
             if (start.x - 1 >= 0)
             {
-                process(new Vector2Int(start.x - 1, start.y), gScore, fScore, cameFrom, openSet, current.Value, goal);
+                process(edgeWeight, new Vector2Int(start.x - 1, start.y), gScore, fScore, cameFrom, openSet, current.Value, goal);
             }
             if (start.x + 1 < tilemapDimensions.x)
             {
-                process(new Vector2Int(start.x + 1, start.y), gScore, fScore, cameFrom, openSet, current.Value, goal);
+                process(edgeWeight, new Vector2Int(start.x + 1, start.y), gScore, fScore, cameFrom, openSet, current.Value, goal);
             }
             if (start.y - 1 >= 0)
             {
-                process(new Vector2Int(start.x, start.y - 1), gScore, fScore, cameFrom, openSet, current.Value, goal);
+                process(edgeWeight, new Vector2Int(start.x, start.y - 1), gScore, fScore, cameFrom, openSet, current.Value, goal);
             }
             if (start.y + 1 < tilemapDimensions.y)
             {
-                process(new Vector2Int(start.x, start.y + 1), gScore, fScore, cameFrom, openSet, current.Value, goal);
+                process(edgeWeight, new Vector2Int(start.x, start.y + 1), gScore, fScore, cameFrom, openSet, current.Value, goal);
             }
         }
 
